a09d65edbedfaeff3aeb906fbca0c7811edfc946
Hunter2718 <benmet2.7@gmail.com>
2025-05-13 21:53:14 -0500

    feat: Read from multiple files and from stdin if no files are present

--- kitten/src/main.rs
+++ kitten/src/main.rs
@@ -1,23 +1,35 @@
 use std::env;
 use std::fs;
-use std::io::{self, Error};
+use std::io::{self, Error, BufRead};
 use std::path::Path;
 
 fn main() -> Result<(), Error> {
     let args: Vec<String> = env::args().collect();
-
+    
     if args.len() < 2 {
-        eprintln!("Not enough Args");
-        std::process::exit(1);
+
+        // TODO: Why does it not end until I manually put EOF with ctrl + D
+        for line in io::stdin().lock().lines() {
+            let line = line.expect("Failed to read line");
+            println!("{}", line);
+        }
+
+    } else {
+    
+        let mut i = 1;
+
+        while i < args.len() {
+
+        println!("{}",
+                match fs::read_to_string(Path::new(&args[i])) {
+                    Err(e) => return Err(e),
+                    Ok(c) => c,
+             }
+         );
+
+          i += 1;
+        }
     }
-
-    let content = match fs::read_to_string(Path::new(&args[1])) {
-        Err(e) => return Err(e),
-        Ok(c) => c
-    };
-
-
-    println!("{content}");
 
     Ok(())
 }
