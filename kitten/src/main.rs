8e42c2acc4dee4497ecfd411b5384d554626610a
Hunter2718 <benmet2.7@gmail.com>
2025-05-14 17:27:17 -0500

    feat: Exit with error if stdin is TTY and not piped

--- kitten/src/main.rs
+++ kitten/src/main.rs
@@ -1,16 +1,23 @@
 use std::env;
 use std::fs;
-use std::io::{self, Error, BufRead};
+use std::io::{self, Error, BufRead, IsTerminal, Write};
 use std::path::Path;
+use std::os::unix::io::AsRawFd;
 
 fn main() -> Result<(), Error> {
     let args: Vec<String> = env::args().collect();
     
     if args.len() < 2 {
 
-        // TODO: Why does it not end until I manually put EOF with ctrl + D
-        for line in io::stdin().lock().lines() {
-            let line = line.expect("Failed to read line");
+        let stdin = io::stdin();
+
+        if stdin.is_terminal() {
+            eprintln!("Must have a file or stdin with pipes");
+            std::process::exit(1);
+        }
+
+        for line in stdin.lock().lines() {
+            let line = line?;
             println!("{}", line);
         }
 
---
+++ kitten/target/debug/deps/kitten-7e132e9ac4c0b501.d
@@ -0,0 +1,5 @@
+/home/ben/Projects/kitten/kitten/target/debug/deps/kitten-7e132e9ac4c0b501: src/main.rs
+
+/home/ben/Projects/kitten/kitten/target/debug/deps/kitten-7e132e9ac4c0b501.d: src/main.rs
+
+src/main.rs:
